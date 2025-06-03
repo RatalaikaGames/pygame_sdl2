5bd9ee809df31d69fe28971cb477fdeafc659c17
mgambrell <mgambrell@users.noreply.github.com>
2025-06-03 02:14:27 -0400

    fix cython3.bat build error

--- cython3.bat
+++ cython3.bat
@@ -8,6 +8,9 @@ set PYDIR=c:\sdk\python-3.9.10
 set path=%PYDIR%;%PYDIR%Scripts;%PATH%
 set PYTHONPATH=%PYDIR%Lib\site-packages
 
+rem it dies without this
+mkdir pygame_sdl2_windeps
+
 python setup.py build_ext
 
 popd
